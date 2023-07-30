f580a6cdb42ab7c8f0991b7cfef4c9fbf31bbda1
CarlosribesDev <carlosribes.dev@gmail.com>
2023-07-31 00:09:54 +0200

    feature: add swagger

--- src/main/java/com/bunker/bookingback/application/dto/BookingDto.java
+++ src/main/java/com/bunker/bookingback/application/dto/BookingDto.java
@@ -1,5 +1,7 @@
 package com.bunker.bookingback.application.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,10 +16,22 @@ public class BookingDto {
 
     private Long id;
     private LocalDate date;
+
+    @Schema(format = "HH:mm", example = "00:00")
+    @JsonFormat(pattern = "HH:mm")
     private LocalTime time;
+
     private Integer numPlayers;
+
+    @Schema(example = "example name")
     private String name;
+
+    @Schema(example = "example@gmail.com")
     private String email;
+
+    @Schema(example = "000000000")
     private String phone;
+
+    @Schema(type = "string")
     private String comment;
 }
