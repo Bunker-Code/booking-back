92d31a87f3ef64df4251864eb6734f7b9b34a5b5
CarlosribesDev <carlosribes.dev@gmail.com>
2023-08-08 14:45:32 +0200

    feature: add corse config

--- src/main/java/com/bunker/bookingback/infrastructure/configuration/CorsConfig.java
+++ src/main/java/com/bunker/bookingback/infrastructure/configuration/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://booking-back-production.up.railway.app")
+                .allowedOrigins("*")
                 .allowedMethods("*")
                 .allowedHeaders("*");
     }
